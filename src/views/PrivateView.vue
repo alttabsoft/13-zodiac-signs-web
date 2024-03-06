9fe9c2e61a4320df27b5aed4edac20901c783e40
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-03-07 01:02:39 +0900

    Update PrivateView.vue

--- src/views/PrivateView.vue
+++ src/views/PrivateView.vue
@@ -3,6 +3,7 @@
     <h1>This is an Test page</h1>
     <button @click = sendGetHttpRequest>나를 눌러 데이터 정보가 들어오는지 확인하기</button>
     <button @click = sendPostHttpRequest>나를 눌러 데이터 정보가 업로드 되는지 확인하기</button>
+    <button @click = sendPostVideoHttpRequest>나를 눌러 비디오 정보가 업로드 되는지 확인하기</button>
     <button @click = sendGetCSRFRequest>나를 눌러 CSRF 정보가 들어오는지 확인하기</button>
     <div>
       <input type="file" @change="uploadFile" />
@@ -43,6 +44,26 @@ function sendPostHttpRequest(){
   formData.append("file" , props.file!) //이름을 철저히 해놓을것..
   function postHttpRequest() {
     createAxiosPostInstance().post(serverStore.url+ "/upload", formData, {withCredentials: true})
+        .then((res) => {
+          console.log(res.data)
+        })
+  }
+
+  if (getCookie('XSRF-TOKEN') == null){
+    creatAxiosInstanceGetCsrfToken()
+        .then(()=>{
+          postHttpRequest();
+        })} else {
+    postHttpRequest();
+  }
+}
+
+function sendPostVideoHttpRequest(){
+  console.log(props.file)
+  let formData = new FormData();
+  formData.append("file" , props.file!) //이름을 철저히 해놓을것..
+  function postHttpRequest() {
+    createAxiosPostInstance().post(serverStore.url+ "/video", formData, {withCredentials: true})
         .then((res) => {
           console.log(res.data)
         })
